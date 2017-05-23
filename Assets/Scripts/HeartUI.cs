e8122d0f27d4b835952fd69889096e08a8000d8a
Unknown <srl447@nyu.edu>
2017-05-23 16:59:32 -0400

    Health UI

---
+++ Assets/Scripts/HeartUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartUI : MonoBehaviour {
+
+    public Animator[] hearts;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (GameManager.totalHealth > -1)
+        {
+            hearts[GameManager.totalHealth].SetTrigger("Hurt");
+        }
+	}
+}
