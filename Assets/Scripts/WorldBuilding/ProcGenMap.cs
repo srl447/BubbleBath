6afe3911d8c281ad434e86156682f2813cb5394d
Unknown <srl447@nyu.edu>
2017-05-24 18:00:50 -0400

    More Tiles

--- Assets/Scripts/WorldBuilding/ProcGenMap.cs
+++ Assets/Scripts/WorldBuilding/ProcGenMap.cs
@@ -45,7 +45,7 @@ public class ProcGenMap : MonoBehaviour
                 transform.position = transform.position + lastMove; //goes in the same direction as last time
             }
 
-            int randTile = Random.Range(0, tiles.Length + 5);
+            int randTile = Random.Range(0, tiles.Length + 4);
             if (randTile > tiles.Length)
             {
                 GameObject newFloor = Instantiate(tiles[Random.Range(0, tiles.Length)]) as GameObject;//places new floor tile
